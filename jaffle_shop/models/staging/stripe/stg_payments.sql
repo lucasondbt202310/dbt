9fe38951f7fe9f45e64f8741d22c00a07f5d5c1f
lucalenq <lucas.alenquer@gmail.com>
2023-10-31 13:26:42 -0400

    adding my first macro

--- jaffle_shop/models/staging/stripe/stg_payments.sql
+++ jaffle_shop/models/staging/stripe/stg_payments.sql
@@ -2,7 +2,7 @@ select
 	ID as payment_id,
 	ORDERID as order_id,
     status,
-	AMOUNT/100 as amount,
-    PAYMENTMETHOD
+	{{ cents_to_dollars('amount', 2) }} as amount,
+    PAYMENTMETHOD as payment_method
 
 from {{source('stripe','payment')}}
